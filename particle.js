c7375e2940ee9593376bbc7dcd292408e3fe996c
jaguarmouse <kazune.haraguchi.aboon@gmail.com>
2019-06-10 00:41:16 +0900

    increased the number of rays to emit

--- particle.js
+++ particle.js
@@ -3,7 +3,7 @@ class Particle {
     this.pos = createVector(x, y);
     this.rays = [];
 
-    for (let a = 0; a < 360; a += 5) {
+    for (let a = 0; a < 360; a += 1) {
       this.rays.push(new Ray(this.pos.x, this.pos.y, radians(a)));
     }
   }
@@ -25,7 +25,22 @@ class Particle {
   }
 
   emit(walls) {
-    for (const ray of this.rays) {
+    const edges = walls.reduce((arr, cur) => {
+      const v1 = p5.Vector.sub(cur.a, this.pos);
+      const v2 = p5.Vector.sub(cur.b, this.pos);
+      const ray1 = new Ray(this.pos.x, this.pos.y, v1.heading());
+      const ray2 = new Ray(this.pos.x, this.pos.y, v2.heading());
+      arr.push(ray1, ray2);
+      return arr;
+    }, []);
+
+    const projections = [];
+
+    const newRays = this.rays
+      .concat(edges)
+      .sort((a,b) => a.dir.heading() - b.dir.heading());
+
+    for (const ray of newRays) {
       let record = Infinity;
       let closest = null;
       for (const wall of walls) {
@@ -39,9 +54,17 @@ class Particle {
         }
       }
       if (closest) {
-        stroke(255);
+        projections.push(closest);
+        stroke(255, 100);
         line(this.pos.x, this.pos.y, closest.x, closest.y);
       }
     }
+
+    fill(255, 180);
+    beginShape();
+    for (const pt of projections) {
+      vertex(pt.x, pt.y);
+    }
+    endShape();
   }
 }
